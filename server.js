e9833a7bbfd9c7d0904651c2b5d251450c58e6eb
Арсен Полівка <Arsen.Polivka@lnu.edu.ua>
2023-06-23 20:47:28 +0300

    database api added

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
-const app = express();
 const { createClient } = require('@supabase/supabase-js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
 const port = 5000;
 
-const supabaseUrl = 'https://hpqhchsxjpoyxejxombx.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
-app.get('/api', (req, res) => {
-	res.json({"users": ["first", "second"]});
-})
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+app.get('/api', async (req, res) => {
+	const { data, error } = await supabase
+		.from('products')
+		.select('*');
+
+	if (error) return res.status(500).send({ error: error.message });
+
+	return res.send(data);
+});
 
 app.listen(port, () => {
 	console.log(`Server had started on port ${port}`);
