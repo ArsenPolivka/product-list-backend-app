108b9a6871136ff80c417c692662c7a4651ffe71
Арсен Полівка <Arsen.Polivka@lnu.edu.ua>
2023-06-23 23:06:10 +0300

    get and delete endpoint implemented

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const port = 5000;
 
 const supabaseUrl = process.env.SUPABASE_URL;
@@ -12,7 +14,7 @@ const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-app.get('/api', async (req, res) => {
+app.get('/products', async (req, res) => {
 	const { data, error } = await supabase
 		.from('products')
 		.select('*');
@@ -21,6 +23,21 @@ app.get('/api', async (req, res) => {
 
 	return res.send(data);
 });
+
+app.delete('/product/delete/:id', async (req, res) => {
+	const { data, error } = await supabase
+		.from('products')
+		.delete()
+		.eq('id', req.params.id)
+})
+
+app.put('/product/add', async (req, res) => {
+	const { data, error } = await supabase
+		.from('products')
+		.insert([
+			{ some_column: 'someValue', other_column: 'otherValue' },
+		])
+})
 
 app.listen(port, () => {
 	console.log(`Server had started on port ${port}`);
